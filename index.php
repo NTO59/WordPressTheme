81666bc02cfea74afea5ea61e244800f65e456e5
Touzard Nicolas <78793616+NTO59@users.noreply.github.com>
2021-03-31 11:50:15 +0200

    init

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <title>Thème WordPress</title>
+</head>
+<body>
+    <h1><?php bloginfo('name');?></h1>
+    <p><?php bloginfo('description');?></p>
+</body>
+</html>
