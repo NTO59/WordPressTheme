ef190f867ff41e6de136497ec676279388620d79
Touzard Nicolas <78793616+NTO59@users.noreply.github.com>
2021-04-01 16:03:52 +0200

    page.php et banner

--- page.php
+++ page.php
@@ -4,8 +4,12 @@ On affiche cette image sur toute la largeur juste en dessous du menu
 avec la hauteur fixe (300px)
 par dessus l'image , on affiche le titre de la page (centré verticalement et horizontalement) -->
 
+</div>
 
 <div class="containerDevis">
+
+
+
     <?php
     // on recupère l'image à la une de l'article
     $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'large')[0] ?? null;
@@ -19,7 +23,7 @@ par dessus l'image , on affiche le titre de la page (centré verticalement et ho
 
 </div>
 
-
+<div class="container">
 
 
 
